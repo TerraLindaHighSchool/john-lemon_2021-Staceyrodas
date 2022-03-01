ebc3cf2d0c95c5fea7dddf5a8c7469073ce0a87a
Staceyrodas <sr01618@srcs.org>
2022-03-01 15:27:13 -0800

    name of game is find the exit all milestones complete  permission to publish
    name of game is find the exit
    all milestones complete
    permission to publish

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int count { get; private set; }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Line 10 Hitting Pickup");
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+}
